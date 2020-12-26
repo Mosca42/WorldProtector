3b9f55505427ba1120dcaec3c89e7bee5f4b3a75
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2020-12-26 17:20:26 +0100

    add line breaks for easier inspection

--- src/main/java/fr/mosca421/worldprotector/core/FlagsList.java
+++ src/main/java/fr/mosca421/worldprotector/core/FlagsList.java
@@ -8,5 +8,31 @@ import net.minecraft.util.ResourceLocation;
 
 public class FlagsList {
 
-	public static final Set<String> VALID_FLAGS = Sets.newHashSet("break", "place", "explosions", "mob-spawning-monsters", "mob-spawning-animal", "mob-spawning-all", "use", "chest-access", "invincible", "damage-players", "enderpearls", "item-drop", "exp-drop", "creeper-explosions", "other-explosions", "damage-animals", "damage-monsters", "send-chat", "fall-damage", "pickup-item", "exit-message", "enter-message", "exit-message-small", "enter-message-small", "block-enter", "block-exit");
+	public static final Set<String> VALID_FLAGS = Sets.newHashSet(
+			"break",
+			"place",
+			"explosions",
+			"mob-spawning-monsters",
+			"mob-spawning-animal",
+			"mob-spawning-all",
+			"use",
+			"chest-access",
+			"invincible",
+			"damage-players",
+			"enderpearls",
+			"item-drop",
+			"exp-drop",
+			"creeper-explosions",
+			"other-explosions",
+			"damage-animals",
+			"damage-monsters",
+			"send-chat",
+			"fall-damage",
+			"pickup-item",
+			"exit-message",
+			"enter-message",
+			"exit-message-small",
+			"enter-message-small",
+			"block-enter",
+			"block-exit");
 }

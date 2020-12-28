00836aa63dfdb11ac27939105057968f8422bfd1
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2020-12-28 23:07:16 +0100

    refactoring, apply linter and fix related issues

--- src/main/java/fr/mosca421/worldprotector/utils/RegionFlagUtils.java
+++ src/main/java/fr/mosca421/worldprotector/utils/RegionFlagUtils.java
@@ -1,12 +1,12 @@
 package fr.mosca421.worldprotector.utils;
 
 import com.google.common.base.Joiner;
-
-import fr.mosca421.worldprotector.core.FlagsList;
 import fr.mosca421.worldprotector.core.Region;
-import fr.mosca421.worldprotector.core.Saver;
+import fr.mosca421.worldprotector.core.RegionFlag;
+import fr.mosca421.worldprotector.core.RegionSaver;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.server.management.OpEntry;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
@@ -14,33 +14,35 @@ import net.minecraft.util.text.TextFormatting;
 
 import java.util.UUID;
 
-public class FlagsUtils {
+public class RegionFlagUtils {
 
-	public static void getRegionFlags(String regions, ServerPlayerEntity player) {
-		if (Saver.REGIONS.containsKey(regions)) {
-			Region region = Saver.REGIONS.get(regions);
-			player.sendMessage(new TranslationTextComponent(TextFormatting.DARK_RED + regions + " Flags : " + Joiner.on(", ").join(region.getFlags())), player.getUniqueID());
+	private RegionFlagUtils(){}
+
+	public static void getRegionFlags(String regionName, ServerPlayerEntity player) {
+		if (RegionSaver.containsRegion(regionName)) {
+			Region region = RegionSaver.getRegion(regionName);
+			player.sendMessage(new TranslationTextComponent(TextFormatting.DARK_RED + regionName + " Flags : " + Joiner.on(", ").join(region.getFlags())), player.getUniqueID());
 		}
 	}
 
-	public static void addFlag(String regions, ServerPlayerEntity player, String flag) {
-		if (Saver.REGIONS.containsKey(regions)) {
-			Region region = Saver.REGIONS.get(regions);
-			if (FlagsList.VALID_FLAGS.contains(flag)) {
+	public static void addFlag(String regionName, ServerPlayerEntity player, String flag) {
+		if (RegionSaver.containsRegion(regionName)) {
+			Region region = RegionSaver.getRegion(regionName);
+			if (RegionFlag.contains(flag)) {
 				region.addFlag(flag);
 				player.sendMessage(new TranslationTextComponent("message.flags.add", flag), player.getUniqueID());
-				Saver.save();
+				RegionSaver.save();
 			}
 		}
 	}
 
-	public static void removeFlag(String regions, ServerPlayerEntity player, String flag) {
-		if (Saver.REGIONS.containsKey(regions)) {
-			Region region = Saver.REGIONS.get(regions);
-			if (FlagsList.VALID_FLAGS.contains(flag)) {
+	public static void removeFlag(String regionName, ServerPlayerEntity player, String flag) {
+		if (RegionSaver.containsRegion(regionName)) {
+			Region region = RegionSaver.getRegion(regionName);
+			if (RegionFlag.contains(flag)) {
 				if (region.removeFlag(flag)) {
 					player.sendMessage(new TranslationTextComponent("message.flags.remove", flag), player.getUniqueID());
-					Saver.save();
+					RegionSaver.save();
 				} else {
 					player.sendMessage(new TranslationTextComponent("message.flags.unknown", flag), player.getUniqueID());
 				}
@@ -60,14 +62,14 @@ public class FlagsUtils {
 	}
 	
 	public static void giveListFlagsOfRegion(ServerPlayerEntity player){
-		player.sendMessage(new StringTextComponent(TextFormatting.DARK_RED + "Flags : " + Joiner.on(", ").join(FlagsList.VALID_FLAGS)), player.getUniqueID());
+		player.sendMessage(new StringTextComponent(TextFormatting.DARK_RED + "Flags : " + Joiner.on(", ").join(RegionFlag.getFlags())), player.getUniqueID());
 	}
 	
 	public static boolean isOp(PlayerEntity name) {
-		if (ServerLifecycleHooks.getCurrentServer().getPlayerList().getOppedPlayers().getEntry(name.getGameProfile()) != null)
-			if (ServerLifecycleHooks.getCurrentServer().getPlayerList().getOppedPlayers().getEntry(name.getGameProfile()).getPermissionLevel() == 4)
-				return true;
-
+		OpEntry opPlayerEntry = ServerLifecycleHooks.getCurrentServer().getPlayerList().getOppedPlayers().getEntry(name.getGameProfile());
+		if (opPlayerEntry != null) {
+			return opPlayerEntry.getPermissionLevel() == 4;
+		}
 		return false;
 	}
 }

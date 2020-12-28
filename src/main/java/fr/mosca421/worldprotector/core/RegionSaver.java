1f701e9bb833fbbf8e5ab4cbc39f542a904784f6
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2020-12-28 22:28:35 +0100

    rework of region worldsaveddata class

---
+++ src/main/java/fr/mosca421/worldprotector/core/RegionSaver.java
@@ -0,0 +1,106 @@
+package fr.mosca421.worldprotector.core;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import fr.mosca421.worldprotector.WorldProtector;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.storage.DimensionSavedDataManager;
+import net.minecraft.world.storage.WorldSavedData;
+import net.minecraftforge.common.util.Constants.NBT;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+
+public class RegionSaver extends WorldSavedData {
+
+	private static RegionSaver instance;
+	private static final Map<String, Region> Regions = new HashMap<>();
+
+	public RegionSaver() {
+		super("worldprotector");
+	}
+
+	public static void save() {
+		if (instance != null) {
+			instance.markDirty();
+		}
+	}
+
+	public static Collection<Region> getRegions(){
+		return Regions.values();
+	}
+
+	public static Collection<String> getRegionNames(){
+		return Regions.keySet();
+	}
+
+	public static Region removeRegion(String regionName) {
+		return Regions.remove(regionName);
+	}
+
+	public static void replaceRegion(Region newRegion){
+		Region oldRegion = Regions.get(newRegion.getName());
+		oldRegion.getFlags().forEach(newRegion::addFlag);
+		removeRegion(oldRegion.getName());
+		addRegion(newRegion);
+	}
+
+	public static Region getRegion(String regionName){
+		return Regions.get(regionName);
+	}
+
+	public static boolean containsRegion(String regionName){
+		return Regions.containsKey(regionName);
+	}
+
+	public static void addRegion(Region region) {
+		Regions.put(region.getName(), region);
+	}
+
+	@Override
+	public void read(CompoundNBT nbt) {
+		Regions.clear();
+		ListNBT regionsList = nbt.getList("regions", NBT.TAG_COMPOUND);
+		for (int i = 0; i < regionsList.size(); i++) {
+			Region area = new Region(regionsList.getCompound(i));
+			Regions.put(area.getName(), area);
+		}
+	}
+
+	@Override
+	public CompoundNBT write(CompoundNBT compound) {
+		ListNBT regionsList = new ListNBT();
+		for (Region area : Regions.values()) {
+			regionsList.add(area.serializeNBT());
+		}
+		compound.put("regions", regionsList);
+		return compound;
+	}
+
+	/**
+	 * FIXME: only data for overworld is loaded here?
+	 * @param event
+	 */
+	public static void onServerStarting(FMLServerStartingEvent event) {
+		try {
+			ServerWorld world = Objects.requireNonNull(event.getServer().getWorld(World.OVERWORLD));
+			if (!(world).isRemote) {
+				DimensionSavedDataManager storage = world.getSavedData();
+				RegionSaver data = storage.get(RegionSaver::new, "worldprotector");
+				if (data == null) {
+					data = new RegionSaver();
+					storage.set(data);
+					WorldProtector.LOGGER.debug("Loaded dimension regions successfully");
+				}
+				instance = data;
+			}
+		} catch (NullPointerException npe) {
+			WorldProtector.LOGGER.error("Loading dimension regions failed");
+		}
+	}
+
+}

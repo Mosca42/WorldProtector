66f1ffa40211166e913e5f9f2c32b9bdcb267317
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2020-12-30 11:37:58 +0100

    postponed work on EventProtection

--- src/main/java/fr/mosca421/worldprotector/events/EventProtection.java
+++ src/main/java/fr/mosca421/worldprotector/events/EventProtection.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import fr.mosca421.worldprotector.WorldProtector;
 import fr.mosca421.worldprotector.core.Region;
+import fr.mosca421.worldprotector.core.RegionFlag;
 import fr.mosca421.worldprotector.utils.RegionsUtils;
 import net.minecraft.entity.monster.CreeperEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -20,16 +21,18 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = WorldProtector.MODID)
 public class EventProtection {
+
+	private EventProtection(){}
+
 	@SubscribeEvent
 	public static void onPlayerBreakBlock(BreakEvent event) {
-		List<Region> regions = RegionsUtils.getHandlingRegionsFor(event.getPos(), RegionsUtils.getDimension(event.getPlayer().world));
+		PlayerEntity player = event.getPlayer();
+		List<Region> regions = RegionsUtils.getHandlingRegionsFor(event.getPos(), RegionsUtils.getDimension(player.world));
 		for (Region region : regions) {
-			if (region.getFlags().contains("break")) {
-				if (!region.isInPlayerList(event.getPlayer())) {
-					event.getPlayer().sendMessage(new TranslationTextComponent("world.protection.break"), event.getPlayer().getUniqueID());
-					event.setCanceled(true);
-					return;
-				}
+			if (region.getFlags().contains(RegionFlag.BREAK.toString()) && !region.permits(player)) {
+				player.sendMessage(new TranslationTextComponent("world.protection.break"), player.getUniqueID());
+				event.setCanceled(true);
+				return;
 			}
 		}
 	}
@@ -38,16 +41,16 @@ public class EventProtection {
 	public static void onPlayerPlaceBlock(EntityPlaceEvent event) {
 		List<Region> regions = RegionsUtils.getHandlingRegionsFor(event.getPos(), RegionsUtils.getDimension((World) event.getWorld()));
 		for (Region region : regions) {
-			if (region.getFlags().contains("place")) {
+			if (region.getFlags().contains(RegionFlag.PLACE.toString())) {
 				if (event.getEntity() instanceof PlayerEntity) {
-						if (!region.isInPlayerList(((PlayerEntity)event.getEntity()))) {
+						if (!region.permits(((PlayerEntity)event.getEntity()))) {
 						event.getEntity().sendMessage(new TranslationTextComponent("world.protection.place"), event.getEntity().getUniqueID());
 						event.setCanceled(true);
 						return;
 					}
 				} else {
 					event.setCanceled(true);
-					return;	
+					return;
 				}
 			}
 		}
@@ -60,7 +63,7 @@ public class EventProtection {
 			BlockPos pos = it.next();
 			List<Region> regions = RegionsUtils.getHandlingRegionsFor(pos, RegionsUtils.getDimension(event.getWorld()));
 			for (Region region : regions) {
-				if (region.getFlags().contains("explosions")) {
+				if (region.getFlags().contains(RegionFlag.EXPLOSION.toString())) {
 					it.remove();
 					break;
 				}
@@ -73,7 +76,7 @@ public class EventProtection {
 				BlockPos pos = it.next();
 				List<Region> regions = RegionsUtils.getHandlingRegionsFor(pos, RegionsUtils.getDimension(event.getWorld()));
 				for (Region region : regions) {
-					if (region.getFlags().contains("other-explosions")) {
+					if (region.getFlags().contains(RegionFlag.EXPLOSION_OTHER.toString())) {
 						it.remove();
 						break;
 					}
@@ -87,7 +90,7 @@ public class EventProtection {
 				BlockPos pos = it.next();
 				List<Region> regions = RegionsUtils.getHandlingRegionsFor(pos, RegionsUtils.getDimension(event.getWorld()));
 				for (Region region : regions) {
-					if (region.getFlags().contains("creeper-explosions")) {
+					if (region.getFlags().contains(RegionFlag.EXPLOSION_CREEPER.toString())) {
 						it.remove();
 						break;
 					}
@@ -98,15 +101,14 @@ public class EventProtection {
 
 	@SubscribeEvent
 	public static void onBucketFill(FillBucketEvent event) {
+		PlayerEntity player = event.getPlayer();
 		if (event.getTarget() != null) {
 			List<Region> regions = RegionsUtils.getHandlingRegionsFor(new BlockPos(event.getTarget().getHitVec()), RegionsUtils.getDimension(event.getWorld()));
 			for (Region region : regions) {
-				if (region.getFlags().contains("place")) {
-					if (!region.isInPlayerList(event.getPlayer())) {
-						event.getPlayer().sendMessage(new TranslationTextComponent("world.protection.place"), event.getPlayer().getUniqueID());
-						event.setCanceled(true);
-						return;
-					}
+				if (region.getFlags().contains(RegionFlag.PLACE.toString()) && !region.permits(player)) {
+					player.sendMessage(new TranslationTextComponent("world.protection.place"), player.getUniqueID());
+					event.setCanceled(true);
+					return;
 				}
 			}
 		}

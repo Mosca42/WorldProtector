e79e3ccaaca8602ec58ec4aa748021f2790cb73a
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2020-12-30 17:56:06 +0100

    refactor ExpandUtils

--- src/main/java/fr/mosca421/worldprotector/utils/ExpandUtils.java
+++ src/main/java/fr/mosca421/worldprotector/utils/ExpandUtils.java
@@ -3,38 +3,41 @@ package fr.mosca421.worldprotector.utils;
 import fr.mosca421.worldprotector.items.RegionStick;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 
-import java.util.UUID;
+import static fr.mosca421.worldprotector.utils.MessageUtils.*;
 
 public class ExpandUtils {
+
+	private ExpandUtils() {}
 	
 	public static void giveHelpMessage(ServerPlayerEntity player) {
-		UUID playerUUID = player.getUniqueID();
-		player.sendMessage(new StringTextComponent(""), playerUUID);
-		player.sendMessage(new TranslationTextComponent(TextFormatting.BLUE + "==WorldProtector Help=="), playerUUID);
-		player.sendMessage(new TranslationTextComponent("help.expand.1"), playerUUID);
-		player.sendMessage(new TranslationTextComponent(TextFormatting.BLUE + "==WorldProtector Help=="), playerUUID);
+		sendMessage(player, "");
+		sendMessage(player, new TranslationTextComponent(TextFormatting.BLUE + "==WorldProtector Help=="));
+		sendMessage(player,"help.expand.1");
+		sendMessage(player, new TranslationTextComponent(TextFormatting.BLUE + "==WorldProtector Help=="));
 	}
 
 	public static void expandVert(ServerPlayerEntity player, ItemStack item) {
 		if (item.getItem() instanceof RegionStick) {
-			if (item.hasTag()) {
-				if (item.getTag().getBoolean("valide")) {
-					item.getTag().putDouble("y1", 0);
-					item.getTag().putDouble("y2", 255);
-					player.sendStatusMessage(new TranslationTextComponent("message.itemhand.expand"), true);
+			if (item.getTag() != null) {
+				CompoundNBT itemTag = item.getTag();
+				if (item.hasTag()) {
+					if (itemTag.getBoolean("valide")) {
+						itemTag.putDouble("y1", 0);
+						itemTag.putDouble("y2", 255);
+						sendMessage(player, "message.itemhand.expand");
+					} else {
+						sendMessage(player, "message.itemhand.choose");
+					}
 				} else {
-
-					player.sendMessage(new TranslationTextComponent("message.itemhand.choose"), player.getUniqueID());
+					sendMessage(player, "message.itemhand.choose");
 				}
-			} else {
-				player.sendMessage(new TranslationTextComponent("message.itemhand.choose"), player.getUniqueID());
 			}
 		} else {
-			player.sendMessage(new TranslationTextComponent("message.itemhand.take"), player.getUniqueID());
+			sendMessage(player, "message.itemhand.take");
 		}
 	}
 }

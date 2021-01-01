41404f837966155432e95c3032a5afce50e106af
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2021-01-01 16:40:32 +0100

    fix chest-access. now works for (hopefully) all blocks and entities with permanent inventory

--- src/main/java/fr/mosca421/worldprotector/event/EventInteract.java
+++ src/main/java/fr/mosca421/worldprotector/event/EventInteract.java
@@ -5,10 +5,12 @@ import fr.mosca421.worldprotector.core.Region;
 import fr.mosca421.worldprotector.core.RegionFlag;
 import fr.mosca421.worldprotector.util.MessageUtils;
 import fr.mosca421.worldprotector.util.RegionUtils;
+import net.minecraft.entity.item.minecart.ContainerMinecartEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.tileentity.EnderChestTileEntity;
+import net.minecraft.tileentity.LecternTileEntity;
 import net.minecraft.tileentity.LockableTileEntity;
 import net.minecraft.util.Hand;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -21,29 +23,54 @@ public class EventInteract {
 	private EventInteract(){}
 
 	@SubscribeEvent
-	public static void onPlayerInteractEvent(PlayerInteractEvent.RightClickBlock event) {
+	public static void onPlayerRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
 		List<Region> regions = RegionUtils.getHandlingRegionsFor(event.getPos(), RegionUtils.getDimension(event.getWorld()));
-		for (Region region : regions) {
-			PlayerEntity player = event.getPlayer();
-			boolean containsUse = region.getFlags().contains(RegionFlag.USE.toString());
-			boolean containsChestAccess = region.getFlags().contains(RegionFlag.CHEST_ACCESS.toString());
-			boolean isLockableTileEntity = (event.getWorld().getTileEntity(event.getPos()) instanceof LockableTileEntity);
-			boolean playerHasPermission = region.permits(player);
+		if (!event.getWorld().isRemote) {
+			for (Region region : regions) {
+				PlayerEntity player = event.getPlayer();
+				boolean containsUse = region.getFlags().contains(RegionFlag.USE.toString());
+				boolean containsChestAccess = region.getFlags().contains(RegionFlag.CHEST_ACCESS.toString());
+				boolean isLockableTileEntity = (event.getWorld().getTileEntity(event.getPos()) instanceof LockableTileEntity);
+				boolean isEnderChest = (event.getWorld().getTileEntity(event.getPos()) instanceof EnderChestTileEntity);
+				boolean isLectern = (event.getWorld().getTileEntity(event.getPos()) instanceof LecternTileEntity);
+				boolean isContainer = isLectern || isEnderChest || isLockableTileEntity;
+				boolean playerHasPermission = region.permits(player);
 
-			if (containsUse && !isLockableTileEntity && !playerHasPermission) {
-				event.setCanceled(true);
-				MessageUtils.sendMessage(player, "world.interact.use");
-				return;
-			}
-
-			if (containsChestAccess && !playerHasPermission) {
-				event.setCanceled(true);
-				if (event.getHand() == Hand.MAIN_HAND) {
-					// FIXME: message is send twice for unknown reason
+				if (containsUse && !isLockableTileEntity && !playerHasPermission) {
+					event.setCanceled(true);
 					MessageUtils.sendMessage(player, "world.interact.use");
+					return;
 				}
-				return;
 
+				if (containsChestAccess && !playerHasPermission && isContainer) {
+					event.setCanceled(true);
+					// Shift click block placement on containers not possible
+					if (event.getHand() == Hand.MAIN_HAND) {
+						MessageUtils.sendMessage(player, "world.interact.container");
+					}
+					return;
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public static void onPlayerEntityInteract(PlayerInteractEvent.EntityInteract event) {
+		List<Region> regions = RegionUtils.getHandlingRegionsFor(event.getPos(), RegionUtils.getDimension(event.getWorld()));
+		if (!event.getWorld().isRemote) {
+			for (Region region : regions) {
+				PlayerEntity player = event.getPlayer();
+				boolean containsChestAccess = region.getFlags().contains(RegionFlag.CHEST_ACCESS.toString());
+				boolean playerHasPermission = region.permits(player);
+				boolean isMinecartContainer = event.getTarget() instanceof ContainerMinecartEntity;
+
+				if (containsChestAccess && !playerHasPermission && isMinecartContainer) {
+					event.setCanceled(true);
+					if (event.getHand() == Hand.MAIN_HAND) {
+						MessageUtils.sendMessage(player, "world.interact.container");
+					}
+					return;
+				}
 			}
 		}
 	}

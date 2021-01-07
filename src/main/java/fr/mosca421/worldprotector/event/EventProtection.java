d1158bd4189c7c8d547a1712403b629831c6c98c
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2021-01-07 09:05:38 +0100

    add feature to prevent explosives from being primed in first place and a feature to prevent player from creating paths, stripping wood and till farmland

--- src/main/java/fr/mosca421/worldprotector/event/EventProtection.java
+++ src/main/java/fr/mosca421/worldprotector/event/EventProtection.java
@@ -7,12 +7,17 @@ import fr.mosca421.worldprotector.WorldProtector;
 import fr.mosca421.worldprotector.core.Region;
 import fr.mosca421.worldprotector.core.RegionFlag;
 import fr.mosca421.worldprotector.util.RegionUtils;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.TNTEntity;
 import net.minecraft.entity.monster.CreeperEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.tileentity.DispenserTileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.entity.player.FillBucketEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import net.minecraftforge.event.world.BlockEvent.EntityPlaceEvent;
 import net.minecraftforge.event.world.ExplosionEvent;
@@ -62,6 +67,27 @@ public class EventProtection {
 		}
 	}
 
+
+	@SubscribeEvent
+	public static void onExplosionStarted(ExplosionEvent.Start event) {
+		if (!event.getWorld().isRemote) {
+			List<Region> regions = RegionUtils.getHandlingRegionsFor(new BlockPos(event.getExplosion().getPosition()), RegionUtils.getDimension(event.getWorld()));
+			if (event.getExplosion().getExplosivePlacedBy() instanceof PlayerEntity) {
+				PlayerEntity player = (PlayerEntity) event.getExplosion().getExplosivePlacedBy();
+				for (Region region : regions) {
+					boolean cancelEvent = region.containsFlag(RegionFlag.IGNITE_EXPLOSIVES) && !region.permits(player);
+					event.setCanceled(cancelEvent);
+					if (cancelEvent) {
+						sendMessage(player, "world.protection.ignitetnt");
+					}
+				}
+			} else {
+				// Projectile or other TNT, or [.?.]
+			}
+		}
+	}
+
+
 	/**
 	 * Checks is any region contains the specified flag
 	 * @param regions regions to check for
@@ -97,6 +123,37 @@ public class EventProtection {
 		}
 		if (explosionTriggeredByCreeper) {
 			event.getAffectedBlocks().removeAll(filterExplosionAffectedBlocks(event, RegionFlag.EXPLOSION_CREEPER.toString()));
+		}
+	}
+
+	@SubscribeEvent
+	public static void onPlayerUseToolSecondary(BlockEvent.BlockToolInteractEvent event) {
+		if (!event.getWorld().isRemote()) {
+			PlayerEntity player = event.getPlayer();
+			List<Region> regions = RegionUtils.getHandlingRegionsFor(event.getPos(), RegionUtils.getDimension(player.getEntityWorld()));
+			for (Region region : regions){ // iterate through regions, if a region contains a specified flag, cancel event
+				boolean playerNotPermitted = !region.permits(player);
+				if (region.containsFlag(RegionFlag.TOOL_SECONDARY_USE) && playerNotPermitted) {
+					event.setCanceled(true);
+					sendMessage(player,   "world.protection.toolsecondary");
+					return;
+				}
+				if (event.getToolType() == ToolType.AXE && region.containsFlag(RegionFlag.AXE_STRIP) && playerNotPermitted) {
+					event.setCanceled(true);
+					sendMessage(player,   "world.protection.stripwood");
+					return;
+				}
+				if (event.getToolType() == ToolType.HOE && region.containsFlag(RegionFlag.HOE_TILL) && playerNotPermitted) {
+					event.setCanceled(true);
+					sendMessage(player,   "world.protection.tillfarmland");
+					return;
+				}
+				if (event.getToolType() == ToolType.SHOVEL && region.containsFlag(RegionFlag.SHOVEL_PATH) && playerNotPermitted) {
+					event.setCanceled(true);
+					sendMessage(player,   "world.protection.shovelpath");
+					return;
+				}
+			}
 		}
 	}
 

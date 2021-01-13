9911d11225a147a189da8e44464d1b8a0e4e13db
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2021-01-13 13:47:51 +0100

    teleportation event now includes shulkers and enderman

--- src/main/java/fr/mosca421/worldprotector/event/EventTeleport.java
+++ src/main/java/fr/mosca421/worldprotector/event/EventTeleport.java
@@ -3,10 +3,13 @@ package fr.mosca421.worldprotector.event;
 import fr.mosca421.worldprotector.WorldProtector;
 import fr.mosca421.worldprotector.core.Region;
 import fr.mosca421.worldprotector.core.RegionFlag;
+import fr.mosca421.worldprotector.util.MessageUtils;
 import fr.mosca421.worldprotector.util.RegionUtils;
+import net.minecraft.entity.monster.EndermanEntity;
+import net.minecraft.entity.monster.ShulkerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.IWorld;
 import net.minecraftforge.event.entity.living.EnderTeleportEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,7 +17,6 @@ import net.minecraftforge.fml.common.Mod;
 import java.util.List;
 
 @Mod.EventBusSubscriber(modid = WorldProtector.MODID)
-
 public class EventTeleport {
 
     private EventTeleport() {
@@ -22,42 +24,71 @@ public class EventTeleport {
 
     @SubscribeEvent
     public static void onEnderTeleportTo(EnderTeleportEvent event) {
-        if (!event.getEntity().world.isRemote) {
+        IWorld world = event.getEntity().world;
+        if (!world.isRemote()) {
             BlockPos teleportTarget = new BlockPos(event.getTargetX(), event.getTargetY(), event.getTargetZ());
             BlockPos teleportSource = event.getEntity().getPosition();
-            List<Region> regionsTo = RegionUtils.getHandlingRegionsFor(teleportTarget, RegionUtils.getDimension(event.getEntity().world));
+            List<Region> regionsTo = RegionUtils.getHandlingRegionsFor(teleportTarget, world);
+            List<Region> regionsFrom = RegionUtils.getHandlingRegionsFor(teleportSource, world);
+            // handle player teleportation using ender pearls
             if (event.getEntityLiving() instanceof PlayerEntity) {
-                PlayerEntity player = (PlayerEntity) event.getEntityLiving();
-                for (Region region : regionsTo) {
-                    if (region.containsFlag(RegionFlag.USE_ENDERPEARL_TO.toString()) && !region.permits(player)) {
-                        event.setCanceled(true);
-                        // refund enderpearl
-                        int count = player.getHeldItem(player.getActiveHand()).getCount();
-                        player.getHeldItem(player.getActiveHand()).setCount(count + 1);
-                        player.sendMessage(new TranslationTextComponent("world.ender.player.to"), player.getUniqueID());
-                    }
-                }
+                handlePlayerEnderPearlUse(event, regionsFrom, regionsTo);
+            }
+            // handle enderman teleportation
+            if (event.getEntity() instanceof EndermanEntity) {
+                handleEndermanTp(event, regionsFrom, regionsTo);
+            }
+            // handle shulker teleportation
+            if (event.getEntity() instanceof ShulkerEntity) {
+                handleShulkerTp(event, regionsFrom, regionsTo);
             }
         }
     }
 
-    @SubscribeEvent
-    public static void onEnderTeleportFrom(EnderTeleportEvent event) {
-        if (!event.getEntity().world.isRemote) {
-            BlockPos teleportSource = event.getEntity().getPosition();
-            List<Region> regionsFrom = RegionUtils.getHandlingRegionsFor(teleportSource, RegionUtils.getDimension(event.getEntity().world));
-            if (event.getEntityLiving() instanceof PlayerEntity) {
-                PlayerEntity player = (PlayerEntity) event.getEntityLiving();
-                for (Region region : regionsFrom) {
-                    if (region.containsFlag(RegionFlag.USE_ENDERPEARL_FROM.toString()) && !region.permits(player)) {
-                        event.setCanceled(true);
-                        // refund enderpearl
-                        int count = player.getHeldItem(player.getActiveHand()).getCount();
-                        player.getHeldItem(player.getActiveHand()).setCount(count + 1);
-                        player.sendMessage(new TranslationTextComponent("world.ender.player.from"), player.getUniqueID());
-                    }
-                }
-            }
+    private static void handlePlayerEnderPearlUse(EnderTeleportEvent event, List<Region> regionsFrom, List<Region> regionsTo) {
+        PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+        boolean playerTpToProhibited = regionsTo.stream()
+                .anyMatch(region -> region.containsFlag(RegionFlag.USE_ENDERPEARL_TO_REGION) && region.forbids(player));
+        if (playerTpToProhibited) {
+            MessageUtils.sendMessage(player, "message.event.teleport.ender_pearl.to_region");
+        }
+        boolean playerTpFromProhibited = regionsFrom.stream()
+                .anyMatch(region -> region.containsFlag(RegionFlag.USE_ENDERPEARL_FROM_REGION) && region.forbids(player));
+        if (playerTpFromProhibited) {
+            MessageUtils.sendMessage(player, "message.event.teleport.ender_pearl.from_region");
+        }
+
+        if (playerTpFromProhibited || playerTpToProhibited) {
+            event.setCanceled(true);
+            // refund pearl
+            int count = player.getHeldItem(player.getActiveHand()).getCount();
+            player.getHeldItem(player.getActiveHand()).setCount(count + 1);
+        }
+    }
+
+    private static void handleShulkerTp(EnderTeleportEvent event, List<Region> regionsFrom, List<Region> regionsTo) {
+        boolean shulkerTpFromProhibited = regionsFrom.stream()
+                .anyMatch(region -> region.containsFlag(RegionFlag.SHULKER_TELEPORT_FROM_REGION));
+        if (shulkerTpFromProhibited) {
+            event.setCanceled(true);
+        }
+        boolean shulkerTpToProhibited = regionsTo.stream()
+                .anyMatch(region -> region.containsFlag(RegionFlag.SHULKER_TELEPORT_TO_REGION));
+        if (shulkerTpToProhibited) {
+            event.setCanceled(true);
+        }
+    }
+
+    private static void handleEndermanTp(EnderTeleportEvent event, List<Region> regionsFrom, List<Region> regionsTo) {
+        boolean endermanTpFromProhibited = regionsFrom.stream()
+                .anyMatch(region -> region.containsFlag(RegionFlag.ENDERMAN_TELEPORT_FROM_REGION));
+        if (endermanTpFromProhibited) {
+            event.setCanceled(true);
+        }
+        boolean endermanTpToProhibited = regionsTo.stream()
+                .anyMatch(region -> region.containsFlag(RegionFlag.ENDERMAN_TELEPORT_TO_REGION));
+        if (endermanTpToProhibited) {
+            event.setCanceled(true);
         }
     }
 }

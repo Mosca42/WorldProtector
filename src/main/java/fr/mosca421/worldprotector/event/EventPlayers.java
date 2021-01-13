eb2de5f62384235e2be7dded183b16192bf1548f
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2021-01-13 14:05:36 +0100

    add event for mounting entites and level-change

--- src/main/java/fr/mosca421/worldprotector/event/EventPlayers.java
+++ src/main/java/fr/mosca421/worldprotector/event/EventPlayers.java
@@ -4,8 +4,10 @@ import fr.mosca421.worldprotector.WorldProtector;
 import fr.mosca421.worldprotector.core.Region;
 import fr.mosca421.worldprotector.core.RegionFlag;
 import fr.mosca421.worldprotector.item.ItemRegionMarker;
+import fr.mosca421.worldprotector.util.MessageUtils;
 import fr.mosca421.worldprotector.util.RegionFlagUtils;
 import fr.mosca421.worldprotector.util.RegionUtils;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.AirItem;
@@ -13,6 +15,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.AnvilUpdateEvent;
 import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.event.entity.EntityMountEvent;
 import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
@@ -20,6 +23,7 @@ import net.minecraftforge.event.entity.player.AnvilRepairEvent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.ItemPickupEvent;
+import net.minecraftforge.event.entity.player.PlayerXpEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -78,6 +82,50 @@ public class EventPlayers {
 	}
 
 	@SubscribeEvent
+	public static void onPlayerLevelChange(PlayerXpEvent.LevelChange event){
+		if (!event.getPlayer().world.isRemote) {
+			PlayerEntity player = event.getPlayer();
+			boolean isLevelChangeProhibithed = RegionUtils.isPlayerActionProhibited(event.getPlayer().getPosition(), player, RegionFlag.LEVEL_CHANGE);
+			if (isLevelChangeProhibithed) {
+				event.setCanceled(true);
+				MessageUtils.sendMessage(player, "message.event.player.level_change");
+			}
+		}
+	}
+
+	/*
+	@SubscribeEvent
+	public static void onPlayerXPChange(PlayerXpEvent.XpChange event){
+		if (!event.getPlayer().world.isRemote) {
+			PlayerEntity player = event.getPlayer();
+			boolean isXpChangeProhibithed = RegionUtils.isPlayerActionProhibited(event.getPlayer().getPosition(), player, RegionFlag.EXP_CHANGE);
+			if (isXpChangeProhibithed) {
+				event.setCanceled(true);
+				// TODO: Seems to not work as it should
+				MessageUtils.sendMessage(player, "message.protection.player.level_change");
+			}
+		}
+	}
+	*/
+
+	/*
+	@SubscribeEvent
+	public static void onPlayerXpPickup(PlayerXpEvent.PickupXp event){
+		if (!event.getPlayer().world.isRemote) {
+			PlayerEntity player = event.getPlayer();
+			boolean isXpPickupProhibithed = RegionUtils.isPlayerActionProhibited(event.getPlayer().getPosition(), player, RegionFlag.EXP_PICKUP);
+			if (isXpPickupProhibithed) {
+				// TODO: Seems to not work as it should
+				event.setCanceled(true);
+				// MessageUtils.sendMessage(player, "message.protection.player.exp_change");
+			}
+		}
+	}
+	*/
+
+
+	// replace with AttackEntityEvent for players and use LivingHurtEvent for enitites
+	@SubscribeEvent
 	public static void onHurt(LivingHurtEvent event) {
 		if (event.getEntityLiving() instanceof ServerPlayerEntity) {
 			ServerPlayerEntity player = (ServerPlayerEntity) event.getEntityLiving();
@@ -113,6 +161,7 @@ public class EventPlayers {
 			ItemStack leftIn = event.getItemInput();
 			PlayerEntity player = event.getPlayer();
 			if (leftIn.getItem() instanceof ItemRegionMarker && rightIn.getItem() instanceof AirItem) {
+				// TODO: spaces are not allowed
 				String regionName = event.getItemResult().getDisplayName().getString();
 				if (!player.hasPermissionLevel(4) || !player.isCreative()) {
 					sendMessage(player,   "message.region.define.denied");
@@ -138,13 +187,38 @@ public class EventPlayers {
 	}
 
 	@SubscribeEvent
-	public static void playerDropItem(ItemTossEvent event) {
+	public static void onPlayerDropItem(ItemTossEvent event) {
 		List<Region> regions = RegionUtils.getHandlingRegionsFor(event.getPlayer().getPosition(), RegionUtils.getDimension(event.getPlayer().world));
 		for (Region region : regions) {
 			if (region.containsFlag(RegionFlag.ITEM_DROP.toString()) && !RegionUtils.isInRegion(region.getName(), event.getPlayer())) {
 				event.setCanceled(true);
 				event.getPlayer().inventory.addItemStackToInventory(event.getEntityItem().getItem());
 				event.getPlayer().sendMessage(new TranslationTextComponent("world.drop.player"), event.getPlayer().getUniqueID());
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public static void onEntityMountAttempt(EntityMountEvent event) {
+		if (!event.getWorldObj().isRemote) {
+			Entity entityBeingMounted = event.getEntityBeingMounted();
+			// could be mob that dismounts because entity being mounted dies?
+			boolean playerAttemptsMounting = event.getEntityMounting() instanceof PlayerEntity;
+			if (playerAttemptsMounting) {
+				PlayerEntity player = (PlayerEntity) event.getEntityMounting();
+				List<Region> regions = RegionUtils.getHandlingRegionsFor(entityBeingMounted, event.getWorldObj());
+				for (Region region : regions) {
+					/*
+					if (event.isDismounting() && region.containsFlag(RegionFlag.ANIMAL_UNMOUNTING) && region.forbids(player)) {
+						event.setCanceled(true); // Does not correctly unmount player - TODO: report bug to forge?
+						sendMessage(player, "message.event.player.unmount");
+					}
+					*/
+					if (event.isMounting() && region.containsFlag(RegionFlag.ANIMAL_MOUNTING) && region.forbids(player)) {
+						event.setCanceled(true);
+						sendMessage(player, "message.event.player.mount");
+					}
+				}
 			}
 		}
 	}

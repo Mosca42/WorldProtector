23963366d5cf99e409f3a7b0861ba16c5c250113
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2021-01-14 14:48:22 +0100

    fix use flag to be more consistent with 1.15.2 behavior and add possibility to place blocks when shift clicking when using use and container access flags

--- src/main/java/fr/mosca421/worldprotector/event/EventInteract.java
+++ src/main/java/fr/mosca421/worldprotector/event/EventInteract.java
@@ -4,13 +4,13 @@ import fr.mosca421.worldprotector.WorldProtector;
 import fr.mosca421.worldprotector.core.Region;
 import fr.mosca421.worldprotector.core.RegionFlag;
 import fr.mosca421.worldprotector.util.MessageUtils;
+import fr.mosca421.worldprotector.util.PlayerUtils;
 import fr.mosca421.worldprotector.util.RegionUtils;
-import net.minecraft.entity.Entity;
+import net.minecraft.block.*;
 import net.minecraft.entity.item.minecart.ContainerMinecartEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tileentity.*;
 import net.minecraft.util.Hand;
-import net.minecraftforge.event.entity.EntityMountEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -28,35 +28,35 @@ public class EventInteract {
 		if (!event.getWorld().isRemote) {
 			for (Region region : regions) {
 				PlayerEntity player = event.getPlayer();
-				TileEntity target = event.getWorld().getTileEntity(event.getPos());
-				boolean containsUse = region.containsFlag(RegionFlag.USE.toString());
-				boolean containsChestAccess = region.containsFlag(RegionFlag.CHEST_ACCESS);
-				boolean containsEnderChestAccess = region.containsFlag(RegionFlag.ENDER_CHEST_ACCESS);
-				boolean isLockableTileEntity = target instanceof LockableTileEntity;
-				boolean isEnderChest = target instanceof EnderChestTileEntity;
-				boolean isLectern = target instanceof LecternTileEntity;
-				boolean isTrappedChest = target instanceof TrappedChestTileEntity;
-				boolean isContainer = isLectern || isLockableTileEntity;
-				boolean playerHasPermission = region.permits(player);
+				TileEntity targetEntity = event.getWorld().getTileEntity(event.getPos());
+				boolean isLockableTileEntity = targetEntity instanceof LockableTileEntity;
+				boolean isEnderChest = targetEntity instanceof EnderChestTileEntity;
+				boolean isContainer = targetEntity instanceof LecternTileEntity || isLockableTileEntity;
+				boolean isPlayerProhibited = region.forbids(player);
 
-				if (containsEnderChestAccess && isEnderChest && region.forbids(player)) {
-					event.setCanceled(true);
-					MessageUtils.sendMessage(player, "message.event.interact.access_ender_chest");
-				}
-				// TODO: use flag not covering tripwire and pressure plate
-				// FIXME: Prevents block placement and general block interaction - should only disable button, lever, door, etc usage
-				if (containsUse && (!isLockableTileEntity || isTrappedChest) && !playerHasPermission) {
-					event.setCanceled(true);
-					MessageUtils.sendMessage(player, "message.event.interact.use");
-					return;
-				}
-				if (containsChestAccess && !playerHasPermission && isContainer) {
-					event.setCanceled(true);
-					// Shift click block placement on containers not possible
-					if (event.getHand() == Hand.MAIN_HAND) {
-						MessageUtils.sendMessage(player, "message.event.interact.access_container");
+				// check for trapped chest
+				if (region.containsFlag(RegionFlag.USE) && !isLockableTileEntity && isPlayerProhibited) {
+					if (!PlayerUtils.isSneaking()) {
+						event.setCanceled(true);
+						MessageUtils.sendMessage(player, "message.event.interact.use");
+						return;
 					}
-					return;
+				}
+				// check for ender chest access
+				if (region.containsFlag(RegionFlag.ENDER_CHEST_ACCESS) && isEnderChest && isPlayerProhibited) {
+					if (!PlayerUtils.isSneaking()) {
+						event.setCanceled(true);
+						MessageUtils.sendMessage(player, "message.event.interact.access_ender_chest");
+						return;
+					}
+				}
+				// check for container access
+				if (region.containsFlag(RegionFlag.CONTAINER_ACCESS) && isContainer && isPlayerProhibited) {
+					if (!PlayerUtils.isSneaking()) {
+						event.setCanceled(true);
+						MessageUtils.sendMessage(player, "message.event.interact.access_container");
+						return;
+					}
 				}
 			}
 		}
@@ -68,7 +68,7 @@ public class EventInteract {
 		if (!event.getWorld().isRemote) {
 			for (Region region : regions) {
 				PlayerEntity player = event.getPlayer();
-				boolean containsChestAccess = region.containsFlag(RegionFlag.CHEST_ACCESS);
+				boolean containsChestAccess = region.containsFlag(RegionFlag.CONTAINER_ACCESS);
 				boolean playerHasPermission = region.permits(player);
 				boolean isMinecartContainer = event.getTarget() instanceof ContainerMinecartEntity;
 

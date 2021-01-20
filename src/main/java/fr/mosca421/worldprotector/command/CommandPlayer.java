9792d86b9e57ab68858a559de9a47db2e1065b6c
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2021-01-20 22:26:55 +0100

    readd command functions to add and remove one player and multiple players

--- src/main/java/fr/mosca421/worldprotector/command/CommandPlayer.java
+++ src/main/java/fr/mosca421/worldprotector/command/CommandPlayer.java
@@ -10,15 +10,18 @@ import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.command.ISuggestionProvider;
 import net.minecraft.command.arguments.EntityArgument;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CommandPlayer {
 
     public static final LiteralArgumentBuilder<CommandSource> PLAYER_COMMAND = register();
 
-    // TODO:
+    // TODO: update help message and readme with new command structure
     public static LiteralArgumentBuilder<CommandSource> register() {
         return Commands.literal(Command.PLAYER.toString())
                 .executes(ctx -> giveHelp(ctx.getSource()))
@@ -27,27 +30,67 @@ public class CommandPlayer {
                 .then(Commands.literal(Command.ADD.toString())
                         .then(Commands.argument(Command.REGION.toString(), StringArgumentType.word())
                                 .suggests((ctx, builder) -> ISuggestionProvider.suggest(RegionManager.get().getAllRegionNames(), builder))
-                                .then(Commands.argument(Command.PLAYER.toString(), EntityArgument.players()) // TODO: suggest all players - players already existing in the region
-                                        .suggests((ctx, builder) -> ISuggestionProvider.suggest(RegionFlag.getFlags(), builder))
-                                        .executes(ctx -> add(ctx.getSource(), StringArgumentType.getString(ctx, Command.REGION.toString()), EntityArgument.getPlayers(ctx, Command.PLAYER.toString()))))))
+                                .then(Commands.argument(Command.PLAYER.toString(), EntityArgument.players())
+                                        .executes(ctx -> addPlayers(ctx.getSource(), StringArgumentType.getString(ctx, Command.REGION.toString()), EntityArgument.getPlayers(ctx, Command.PLAYER.toString()))))))
+                .then(Commands.literal(Command.ADD.toString())
+                        .then(Commands.argument(Command.REGION.toString(), StringArgumentType.word())
+                                .suggests((ctx, builder) -> ISuggestionProvider.suggest(RegionManager.get().getAllRegionNames(), builder))
+                                .then(Commands.argument(Command.PLAYER.toString(), EntityArgument.player())
+                                        .executes(ctx -> addPlayer(ctx.getSource(), StringArgumentType.getString(ctx, Command.REGION.toString()), EntityArgument.getPlayer(ctx, Command.PLAYER.toString()))))))
                 .then(Commands.literal(Command.REMOVE.toString())
                         .then(Commands.argument(Command.REGION.toString(), StringArgumentType.word())
                                 .suggests((ctx, builder) -> ISuggestionProvider.suggest(RegionManager.get().getAllRegionNames(), builder))
-                                .then(Commands.argument(Command.PLAYER.toString(), EntityArgument.players()) // TODO: suggest only players already existing in the region
-                                        .suggests((ctx, builder) -> ISuggestionProvider.suggest(RegionManager.get().getRegionFlags(ctx.getArgument(Command.REGION.toString(), String.class), ctx.getSource().getWorld().getDimensionKey()), builder))
-                                        .executes(ctx -> remove(ctx.getSource(), StringArgumentType.getString(ctx, Command.REGION.toString()), EntityArgument.getPlayers(ctx, Command.PLAYER.toString()))))))
+                                .then(Commands.argument(Command.PLAYER.toString(), EntityArgument.players())
+                                        .suggests((ctx, builder) -> ISuggestionProvider.suggest(RegionManager.get().getRegionPlayers(ctx.getArgument(Command.REGION.toString(), String.class)), builder))
+                                        .executes(ctx -> removePlayers(ctx.getSource(), StringArgumentType.getString(ctx, Command.REGION.toString()), EntityArgument.getPlayers(ctx, Command.PLAYER.toString()))))))
+                .then(Commands.literal(Command.REMOVE.toString())
+                        .then(Commands.argument(Command.REGION.toString(), StringArgumentType.word())
+                                .suggests((ctx, builder) -> ISuggestionProvider.suggest(RegionManager.get().getAllRegionNames(), builder))
+                                .then(Commands.argument(Command.PLAYER.toString(), EntityArgument.player())
+                                        .suggests((ctx, builder) -> ISuggestionProvider.suggest(RegionManager.get().getRegionPlayers(ctx.getArgument(Command.REGION.toString(), String.class)), builder))
+                                        .executes(ctx -> removePlayer(ctx.getSource(), StringArgumentType.getString(ctx, Command.REGION.toString()), EntityArgument.getPlayer(ctx, Command.PLAYER.toString()))))))
                 .then(Commands.literal(Command.INFO.toString())
                         .then(Commands.argument(Command.REGION.toString(), StringArgumentType.word())
                                 .suggests((ctx, builder) -> ISuggestionProvider.suggest(RegionManager.get().getAllRegionNames(), builder))
                                 .executes(ctx -> info(ctx.getSource(), StringArgumentType.getString(ctx, Command.REGION.toString())))));
     }
 
-    private static int remove(CommandSource source, String regionName, Collection<ServerPlayerEntity> players) {
+    private static int removePlayer(CommandSource source, String regionName, ServerPlayerEntity player) {
+        try {
+            RegionPlayerUtils.removePlayer(regionName, source.asPlayer(), player);
+        } catch (CommandSyntaxException e) {
+            e.printStackTrace();
+        }
         return 0;
     }
 
-    private static int add(CommandSource source, String regionName, Collection<ServerPlayerEntity> players) {
+    private static int removePlayers(CommandSource source, String regionName, Collection<ServerPlayerEntity> players) {
+        try {
+            List<PlayerEntity> playerList = players.stream().map(player -> (PlayerEntity)player).collect(Collectors.toList());
+            RegionPlayerUtils.removePlayers(regionName, source.asPlayer(), playerList);
+        } catch (CommandSyntaxException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 
+    private static int addPlayer(CommandSource source, String regionName, ServerPlayerEntity player) {
+        try {
+            RegionPlayerUtils.addPlayer(regionName, source.asPlayer(), player);
+        } catch (CommandSyntaxException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    private static int addPlayers(CommandSource source, String regionName, Collection<ServerPlayerEntity> players) {
+        try {
+            List<PlayerEntity> playerList = players.stream().map(player -> (PlayerEntity)player).collect(Collectors.toList());
+            RegionPlayerUtils.addPlayers(regionName, source.asPlayer(), playerList);
+        } catch (CommandSyntaxException e) {
+            e.printStackTrace();
+        }
         return 0;
     }
 
